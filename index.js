e1e41498f6737872e5b7f7b387102cfac67cf675
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-12-01 03:45:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ class Formatter {
 //Write a method static capitalize that takes in a string
 //and capitalizes the first letter.
 static capitalize(string){
-  return string.charAt(0).toUpperCase + string.ice(1)
+  return string.charAt(0).toUpperCase + string.slice(1)
 }
 //Write a method static sanitize that takes in a string and removes all non-alphanumeric characters except for dashes, single quotes and spaces.
 //Write a method static titleize that takes in a string and capitalizes all words in a sentence except

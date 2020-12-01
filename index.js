810ab664a3b0f70b9c117bfc27f7ccd542735a8b
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-12-01 03:49:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ static capitalize( string ) {
 //Write a method static sanitize that takes in a string and
 //removes all non-alphanumeric characters except for dashes,
 //single quotes and spaces.
+static sanitize(string){
+  
+}
 //Write a method static titleize that takes in a string and capitalizes all words in a sentence except
 //the, a, an, but, of, and, for, at, by, and from; and always capitalizes the first word.
 }
